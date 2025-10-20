2555e6d5aa541a04a6a50f6d37745f31a5537ba7
KristianNyborgJespersen <Krelle757@gmail.com>
2025-10-20 14:29:53 +0200

    Fixed icon

--- icons/create_icons.sh
+++ icons/create_icons.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Simple script to create placeholder icon files
-# These are minimal valid PNG files that browsers will accept
-# Create a minimal PNG (1x1 blue pixel) and resize using base64
+# Script to create icon files from SVG using ImageMagick
+# Converts icon.svg to PNG files at different sizes
+
 create_icon() {
     size=$1
-    # This is a base64 encoded 16x16 blue PNG
-    echo "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAKklEQVR42mNgYGD4TwQGlEOSYWCgHIwqGB5g1AZqBqM2UDMYtYGaAQCKBAH/thB+xAAAAABJRU5ErkJggg==" | base64 -d > icon${size}.png
+    echo "Creating icon${size}.png..."
+    convert -background none -resize ${size}x${size} icon.svg icon${size}.png
 }
+
 create_icon 16
 create_icon 48  
 create_icon 128
-echo "Placeholder icons created. For better icons, open generate-icons.html in a browser."
+
+echo "Icons created successfully from icon.svg"
